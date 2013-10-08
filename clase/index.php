45ebba18e0f0707624549987c65d4fae7e679eb0
usuario <usuario@usuario-HP>
2013-10-08 15:46:53 -0400

    Borrados carpetas y archivos innecesarios. Se eliminaron también espacios entre códigos.

--- clase/index.php
+++ clase/index.php
@@ -11,5 +11,4 @@ $pagina1->insertarCuerpo('Esto es una prueba que debe aparecer dentro del cuerpo
 $pagina1->insertarCuerpo('Esto es una prueba que debe aparecer dentro del cuerpo de la página 8');
 $pagina1->insertarCuerpo('Esto es una prueba que debe aparecer dentro del cuerpo de la página 9');
 $pagina1->graficar();
-
 ?>
--- menu/ejemplo1.php
+++ menu/ejemplo1.php
@@ -1,51 +1,39 @@
 <?php
-
 class Opcion {
-
     private $titulo;
     private $enlace;
     private $colorFondo;
-
     public function __construct($tit, $enl, $cfon) {
         $this->titulo = $tit;
         $this->enlace = $enl;
         $this->colorFondo = $cfon;
     }
-
     public function graficar() {
         echo '<a style="background-color:' . $this->colorFondo .
         '" href="' . $this->enlace . '">' . $this->titulo . '</a>';
     }
-
 }
-
 class Menu {
-
     private $opciones = array();
     private $direccion;
-
     public function __construct($dir) {
         $this->direccion = $dir;
     }
-
     public function insertar($op) {
         $this->opciones[] = $op;
     }
-
     private function graficarHorizontal() {
         for ($f = 0; $f < count($this->opciones); $f++) {
             $this->opciones[$f]->graficar();
             echo ' ';
         }
     }
-
     private function graficarVertical() {
         for ($f = 0; $f < count($this->opciones); $f++) {
             $this->opciones[$f]->graficar();
             echo '<br>';
         }
     }
-
     public function graficar() {
         if (strtolower($this->direccion) == "horizontal")
             $this->graficarHorizontal();
@@ -53,9 +41,7 @@ class Menu {
         if (strtolower($this->direccion) == "vertical")
             $this->graficarVertical();
     }
-
 }
-
 $menu1 = new Menu('horizontal');
 $opcion1 = new Opcion('Google', 'http://www.google.com', '#C3D9FF');
 $menu1->insertar($opcion1);
